f2ac279cabc2d810d7d4c8db74b4d440fc180400
Olexandr Maltsev <olexandr.maltsev@gmail.com>
2018-11-08 10:51:21 +0200

    Realises bucket sorting algorithm

--- src/main/academy.softserve/bucketsort/Sorter.java
+++ src/main/academy.softserve/bucketsort/Sorter.java
@@ -1,5 +1,8 @@
 package academy.softserve.bucketsort;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Sorter {
 
     void bucketSort(int[] input) {
@@ -9,7 +12,6 @@ class Sorter {
 
         int max = input[0];
         int min = input[0];
-
         for (int i = 1; i < input.length; i++) {
             if (max < input[i]) {
                 max = input[i];
@@ -23,13 +25,44 @@ class Sorter {
             buckets[i] = new Bucket();
         }
 
-        for (int number: input) {
-            int bucketIndex = (int) ((number - min ) / (max - min + 1.0) * buckets.length);
+        for (int number : input) {
+            int bucketIndex = (int) ((number - min) / (max - min + 1.0) * buckets.length);
             buckets[bucketIndex].bucket.add(number);
         }
 
-        for (Bucket bucket: buckets) {
-            System.out.println(bucket);
+        for (Bucket bucket : buckets) {
+            if (bucket.bucket.size() > 1) {
+                bucket.bucket = sortBucket(bucket.bucket);
+            }
         }
+
+        int[] result = new int[input.length];
+        int resultIndex = 0;
+        for (Bucket bucket : buckets) {
+            if (bucket.bucket.size() > 0) {
+                for (Integer i : bucket.bucket) {
+                    result[resultIndex] = i;
+                    resultIndex++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    private ArrayList<Integer> sortBucket(ArrayList<Integer> bucket) {
+
+        for (int i = 1; i < bucket.size(); i++) {
+            if (bucket.get(i) < bucket.get(i - 1)) {
+                for (int j = i; j > 0; j--) {
+                    if (bucket.get(j) < bucket.get(j - 1)) {
+                        int temp = bucket.get(j);
+                        bucket.set(j, bucket.get(j - 1));
+                        bucket.set(j - 1, temp);
+                    }
+                }
+            }
+        }
+        return bucket;
     }
 }

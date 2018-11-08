58df343928db2b73dab6f41f55909e4739baba5d
Olexandr Maltsev <olexandr.maltsev@gmail.com>
2018-11-08 11:15:59 +0200

    Adds msBits() for calculating bucket Index, changes return value for bucketSort()

--- src/main/academy.softserve/bucketsort/Sorter.java
+++ src/main/academy.softserve/bucketsort/Sorter.java
@@ -5,11 +5,43 @@ import java.util.Arrays;
 
 class Sorter {
 
-    void bucketSort(int[] input) {
+    int[] bucketSort(int[] input) {
         if (input.length == 0 || input.length == 1) {
-            return;
+            return input;
         }
 
+        Bucket[] buckets = new Bucket[input.length]; //buckets initializing
+        for (int i = 0; i < buckets.length; i++) {
+            buckets[i] = new Bucket();
+        }
+
+        for (int number : input) { //put all numbers in buckets
+            int bucketIndex = msBits(input, number);
+            buckets[bucketIndex].bucket.add(number);
+        }
+
+        for (Bucket bucket : buckets) { //sort numbers in buckets
+            if (bucket.bucket.size() > 1) {
+                bucket.bucket = sortBucket(bucket.bucket);
+            }
+        }
+
+        int[] result = new int[input.length];
+        int resultIndex = 0;
+        for (Bucket bucket : buckets) { //combine numbers from buckets
+            if (bucket.bucket.size() > 0) {
+                for (Integer i : bucket.bucket) {
+                    result[resultIndex] = i;
+                    resultIndex++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+        return result;
+    }
+
+    int msBits(int[] input, int number) { //return bucket Index
         int max = input[0];
         int min = input[0];
         for (int i = 1; i < input.length; i++) {
@@ -20,37 +52,12 @@ class Sorter {
             }
         }
 
-        Bucket[] buckets = new Bucket[input.length];
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = new Bucket();
-        }
-
-        for (int number : input) {
-            int bucketIndex = (int) ((number - min) / (max - min + 1.0) * buckets.length);
-            buckets[bucketIndex].bucket.add(number);
-        }
-
-        for (Bucket bucket : buckets) {
-            if (bucket.bucket.size() > 1) {
-                bucket.bucket = sortBucket(bucket.bucket);
-            }
-        }
-
-        int[] result = new int[input.length];
-        int resultIndex = 0;
-        for (Bucket bucket : buckets) {
-            if (bucket.bucket.size() > 0) {
-                for (Integer i : bucket.bucket) {
-                    result[resultIndex] = i;
-                    resultIndex++;
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(result));
+        double sortingFunctionResult = (number - min) / (max - min + 1.0) * input.length;
+        int bucketIndex = (int) sortingFunctionResult;
+        return bucketIndex;
     }
 
-    private ArrayList<Integer> sortBucket(ArrayList<Integer> bucket) {
+    ArrayList<Integer> sortBucket(ArrayList<Integer> bucket) { //insertion sort algorithm for buckets
 
         for (int i = 1; i < bucket.size(); i++) {
             if (bucket.get(i) < bucket.get(i - 1)) {
